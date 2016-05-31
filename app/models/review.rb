eaa9218118fbba039b3d4dba7ecc1fdac43d88af
yumimac <yumi.ko.cheers@gmail.com>
2016-05-31 22:15:04 +1000

    first commit after changing the folder

---
+++ app/models/review.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id         :integer          not null, primary key
+#  rating     :integer
+#  content    :text
+#  user_id    :integer
+#  region_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  avatar     :string
+#
+
+class Review < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
+  belongs_to :user
+  belongs_to :region
+end
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Fishing Review Application
+
+- 'rails 4.0??', 'Bootstrap'
+- 'API : geocoder, gmaps4rails, weather api'
+- 'image uploader'
+- postgresql
+- gem : cloudinary, 
+
+How to use it
+What each pages do
